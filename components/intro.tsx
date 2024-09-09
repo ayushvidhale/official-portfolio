c90aacff41d45c1645b14760e0ee2743b529ce0d
ayushvidhale <ayushvidhale099@gmail.com>
2024-09-09 17:38:27 +0530

    profile update

--- components/intro.tsx
+++ components/intro.tsx
@@ -31,8 +31,8 @@ export default function Intro() {
             }}
           >
             <img
-              src="https://media.licdn.com/dms/image/D4D03AQEGJJZqho8mRw/profile-displayphoto-shrink_800_800/0/1719685950546?e=1727913600&v=beta&t=MQ-yNhqGWeqISutngn9tlyIDPQoA84cmDPuo3wCZ5Aw"
-              alt="Ayush Vidhale"
+              src="./ayush.jpeg"
+              alt=""
               className="h-32 w-32 rounded-full object-cover border-[0.15rem] border-gray-200 shadow-xl"
             />
           </motion.div>
